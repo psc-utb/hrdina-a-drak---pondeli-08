12f3005cd4fc5b9accb1fb692978cdc55dfbb4f8
FunTom <vogeltanz@utb.cz>
2022-02-07 19:52:06 +0100

    Přidejte soubory projektu.

---
+++ hrdina a drak - pondeli 08/Hrdina.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hrdina_a_drak___pondeli_08
+{
+    public class Hrdina
+    {
+        public string Jmeno { get; set; }
+        public double Zdravi { get; set; }
+        public double ZdraviMax { get; set; }
+        public double PoskozeniMax { get; set; }
+        public double ZbrojMax { get; set; }
+
+        public Hrdina(string jmeno, double zdravi, double zdraviMax, double poskozeniMax, double zbrojMax)
+        {
+            Jmeno = jmeno;
+            Zdravi = zdravi;
+            ZdraviMax = zdraviMax;
+            PoskozeniMax = poskozeniMax;
+            ZbrojMax = zbrojMax;
+        }
+
+        public double Utok(Drak oponent)
+        {
+            double hodnotaUtoku = 0;
+
+            Random rnd = new Random();
+            hodnotaUtoku = rnd.NextDouble() * PoskozeniMax;
+            hodnotaUtoku -= oponent.Obrana();
+            if (hodnotaUtoku < 0)
+                hodnotaUtoku = 0;
+            oponent.Zdravi -= hodnotaUtoku;
+            
+            return hodnotaUtoku;
+        }
+
+        public double Obrana()
+        {
+            double hodnotaObrana = 0;
+
+            //dodělat
+
+            return hodnotaObrana;
+        }
+
+        public bool JeZivy()
+        {
+            if (Zdravi > 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
